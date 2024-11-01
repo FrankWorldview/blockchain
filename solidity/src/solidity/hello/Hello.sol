58f64bd3eefb8efc135a17fb23fb91bf09186fc0
Frank <frankcpp@gmail.com>
2024-11-02 03:54:44 +0800

    Add files via upload

---
+++ solidity/src/solidity/hello/Hello.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {console} from "forge-std/console.sol";
+
+contract Hello {
+    constructor() {
+        console.log("Constructor of the Hello contract.");
+    }
+
+    function greet() public pure returns (string memory) {
+        console.log("Hello world!");
+
+        return "Hello world!";
+    }
+
+    function getMyName() public pure returns (string memory) {
+        console.log("Pepe");
+
+        return "Pepe";
+    }
+}
