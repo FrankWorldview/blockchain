9ea0cde17fb2f17603cf5870e4ba2d004e6b9c7a
Frank <frankcpp@gmail.com>
2024-11-03 16:51:59 +0800

    Add files via upload

--- smart_contracts/src/web/hello-app/App.js
+++ smart_contracts/src/web/hello-app/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const { Web3 } = require('web3');
+const helloABI = require('./abi/Hello.json').abi;
+
 function App() {
+  const [text, setText] = useState(null);
+  const [name, setName] = useState(null);
+
+  async function callHello() {
+    try {
+      // Private RPC endpoint.
+      const web3 = new Web3('http://127.0.0.1:8545');
+
+      const addr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+      const hello = new web3.eth.Contract(helloABI, addr);
+
+      const text = await hello.methods.greet().call();
+
+      const name = await hello.methods.getMyName().call();
+
+      setText(text);
+
+      setName(name);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    callHello();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +51,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>greet(): {text}</p>
+        <p>getMyName(): {name}</p>
       </header>
     </div>
   );
