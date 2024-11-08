1065f3b2e1ca2f90f62c4fd04abe3b3002c2ec21
Frank <frankcpp@gmail.com>
2024-11-08 22:06:10 +0800

    Add files via upload

--- smart_contracts/src/web/hello/Hello.js
+++ smart_contracts/src/web/hello/Hello.js
@@ -1,14 +1,15 @@
 const { Web3 } = require('web3');
-const helloABI = require('./abi/Hello.json').abi;
 
 async function main() {
     try {
         // Private RPC endpoint.
         const web3 = new Web3('http://127.0.0.1:8545');
 
-        const addr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        const helloABI = require('./abi/Hello.json').abi;
 
-        const hello = new web3.eth.Contract(helloABI, addr);
+        const helloAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+        const hello = new web3.eth.Contract(helloABI, helloAddr);
 
         const text = await hello.methods.greet().call();
 
